6bdd09c005689a1b16b0753f3a8a6a5ffdf8c472
Guilherme N. Marques <guilherme.n.marques@unesp.br>
2024-06-23 14:13:15 -0300

    Envio com Menu

--- Constants.java
+++ Constants.java
@@ -1,7 +1,7 @@
 public abstract class Constants {
     
-    public static int WIDTH=1200;
-    public static int HEIGHT=700;
+    public static int WIDTH=1300;
+    public static int HEIGHT=800;
     public static String TITLE="Zombie Chaos";
     public static double AMOUNT_OF_TICKS=120.0;
     public static int SHOOTER_WIDTH=332;
